c5807f256f540d3433b6959d258034eadf727bc7
hellwoxx <hellwoxx@c85d109a-158f-11de-8b5c-13accb87f508>
2009-03-21 12:47:14 +0000

    Corrections pour les MV coords.
    Deform à corriger maintenant !

--- mv_coord.m
+++ mv_coord.m
@@ -1,35 +1,72 @@
 function coord = mv_coord(cage,p,verbose)
 % Compute Mean Value Coordinates of a point p within the input cage
+%
+% Input
+%   cage                : dim 2*nb_points
+%   p                   : dim 2*1
+%   verbose (optional)  : boolean
+%
+% Ouptut
+%   coord   : Mean Value coordinates
+
+% Ref. [Hormann, Floater] Mean Value Coordintates for Arbitrary Planar Polygons
 
 % File of the MorphoxX Project
 % @author: JB Fiot (HellWoxX)
 
-V = cage-repmat(p,[1,size(cage,2)]);
+
+% =========================================================================
+%                           Formulas
+% =========================================================================
+% 
+% Let {Vi} be the vertices in the cage
+% Let Ri(p) = ||p-Vi||
+% Let Ai be the signed area of the triangle [p, Vi, Vi+1] at the vertex p
+% Let Bi be the signed area of the triangle [p, Vi-1, Vi+1] at the vertex p
+
+Vrel = cage-repmat(p,[1,size(cage,2)]);
 coord = zeros(1,size(cage,2));
 r=zeros(size(coord));
 A=zeros(size(coord));
 B=zeros(size(coord));
+
+% Compute distances to vertices
 for i=1:length(r)
-    r(i)=norm(V(:,i));
+    r(i)=norm(Vrel(:,i));
 end
+
+% Compute signed triangle areas
 for i=1:length(r)-1
-    A(i)=norm(cross([V(:,i);0],[V(:,i+1);0]));
+    tmp = cross([Vrel(:,i);0],[Vrel(:,i+1);0]);
+    A(i)=tmp(3)/2;
 end
-A(length(r))=norm(cross([V(:,length(r));0],[V(:,1);0]));
+tmp = cross([Vrel(:,length(r));0],[Vrel(:,1);0]);
+A(length(r))=tmp(3)/2;
 
 for i=2:length(r)-1
-    B(i)=norm(cross([V(:,i-1);0],[V(:,i+1);0]));
+    tmp = cross([Vrel(:,i-1);0],[Vrel(:,i+1);0]);
+    B(i)=tmp(3)/2;
 end
-B(1)=norm(cross([V(:,length(r));0],[V(:,2);0]));
-B(length(r))=norm(cross([V(:,length(r)-1);0],[V(:,1);0]));
+tmp = cross([Vrel(:,length(r));0],[Vrel(:,2);0]);
+B(1)=tmp(3)/2;
+tmp = cross([Vrel(:,length(r)-1);0],[Vrel(:,1);0]);
+B(length(r))=tmp(3)/2;
 
-coord = [(r(end)*A(1)-r(1)*B(1)+r(2)*A(end))/(A(end)*A(1)) ,...
-    (r(1:end-2).*A(2:end-1)-r(2:end-1).*B(2:end-1)-r(3:end).*A(1:end-2))./(A(1:end-2).*A(2:end-1)),...
-    (r(end-1)*A(end)-r(end)*B(end)+r(1)*A(end-1))/(A(end)*A(end-1))];
+clear tmp;
 
+% Weight functions
+% weight = [r(1)/(A(end)*A(1)),r(2:end)./(A(1:end-1).*A(2:end))];
+
+% Compute MV Coords
+coord = [(r(end)*A(1)-r(1)*B(1)+r(2)*A(end))/(A(end)*A(1)) ,... % elem #1
+    (r(1:end-2).*A(2:end-1)-r(2:end-1).*B(2:end-1)-r(3:end).*A(1:end-2))./(A(1:end-2).*A(2:end-1)),... % elem #2 to #end-1
+    (r(end-1)*A(end)-r(end)*B(end)+r(1)*A(end-1))/(A(end-1)*A(end))]; % last elem
+
+% Normalization
 coord = coord/sum(coord);
-
-if verbose
-    display(coord);
+if nargin>2
+    if verbose
+        display(coord);
+    end
 end
 end
