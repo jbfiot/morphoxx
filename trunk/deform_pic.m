4f8f9492eb294bd87bb18ff72037b512aab53eb1
hellwoxx <hellwoxx@c85d109a-158f-11de-8b5c-13accb87f508>
2009-03-22 12:00:40 +0000

    Reorga, optim in display_coordinate_value.m and output of MV values for Sonic set.

--- trunk/deform_pic.m
+++ trunk/deform_pic.m
@@ -1,10 +1,10 @@
-function deformed_pic = deform(image,cage,deformed_cage,coord_type,interpolate)
-% function deformed_pic = deform(image,cage,deformed_cage,coord_type,interpolate)
+function deformed_pic = deform_pic(pic,cage,deformed_cage,coord_type,interpolate)
+% function deformed_pic = deform(pic,cage,deformed_cage,coord_type,interpolate)
 % 
 % Deform a picture using the cage deformation and Mean Value Coordinates
 %
 % Input
-%   image           : image to deform
+%   pic             : picture to deform
 %   cage            : original cage
 %   deformed_cage   : deformed cage
 %   coord_type      : type of coordinates to use
@@ -17,18 +17,13 @@ function deformed_pic = deform(image,cage,deformed_cage,coord_type,interpolate)
 % File of the MorphoxX Project
 % @author: JB Fiot (HellWoxX)
 
-[size_x,size_y,nb_channels] = size(image);
-deformed_pic = ones(size(image));
+[size_x,size_y,nb_channels] = size(pic);
+deformed_pic = ones(size(pic));
 in=0;
 out=0;
 for i=1:size_x
     for j=1:size_y
-        switch coord_type
-            case 'MV'
-                coord = mv_coord(deformed_cage,[i;j],0);
-            otherwise
-                error('Unknown coord type');
-        end
+        coord = get_coord(deformed_cage,[i;j],coord_type,0);
 
         before_def_point = cage*coord';
         before_def_x = before_def_point(1);
@@ -39,9 +34,9 @@ for i=1:size_x
         if floor(before_def_x)>0 && floor(before_def_y)>0 && before_def_x<size_x && before_def_y<size_y
             for k=1:nb_channels
                 if interpolate
-                    deformed_pic(i,j,k) = interp2(X,Y,image(floor(before_def_x):floor(before_def_x)+1,floor(before_def_y):floor(before_def_y)+1,k),before_def_x,before_def_y);
+                    deformed_pic(i,j,k) = interp2(X,Y,pic(floor(before_def_x):floor(before_def_x)+1,floor(before_def_y):floor(before_def_y)+1,k),before_def_x,before_def_y);
                 else
-                    deformed_pic(i,j,k) = image(floor(before_def_x),floor(before_def_y),k);
+                    deformed_pic(i,j,k) = pic(floor(before_def_x),floor(before_def_y),k);
                 end
             end
             in = in+1;
--- trunk/display_coordinate_values.m
+++ trunk/display_coordinate_values.m
@@ -7,36 +7,37 @@
 clc; clear; close all;
 
 display('Computing... Please be patient and let Matlab keep the focus!');
-set = 'L Checkerboard';
+
+set = 'Sonic';
+coord_type = 'MV';
+[image,cage_filename] = switchset(set);
+cage = load(cage_filename);
+
 if ~exist('Output','dir')
     mkdir('Output');
 end
 if ~exist(['Output/',set],'dir')
     mkdir(['Output/',set]);
 end
-interpolate = 1;
-coord_type = 'MV';
-[image,cage_filename] = switchset(set);
-cage = load(cage_filename);
 
-coord_values = zeros(size(image,1),size(image,2));
+vertex_nb = size(cage,2);
 
-figure;
+coord_values = zeros(size(image,1),size(image,2),vertex_nb);
 
-for vertex_nb = 1:size(cage,2);
-    for i=1:size(image,1)
-        for j=1:size(image,2)
-            switch coord_type
-                case 'MV'
-                    tmp = mv_coord(cage,[i;j]);
-                    coord_values(i,j) = tmp(vertex_nb);
-            end
-        end
+% Getting coords
+for i=1:size(image,1)
+    for j=1:size(image,2)
+        coord_values(i,j,:) = get_coord(cage,[i;j],coord_type);
     end
+end
 
-    clf; draw_cage(cage,coord_values,vertex_nb); colormap('hot'); colorbar; 
-    name = ['Output/',set,'/', coord_type, ' coord for vertex ',int2str(vertex_nb),'.jpg'];
+% Display and save
+figure;
+for vertex_ind = 1:vertex_nb;
+    clf; draw_cage(cage,coord_values(:,:,vertex_ind)); colormap('hot'); colorbar; 
+    name = ['Output/',set,'/', coord_type, ' coord for vertex ',int2str(vertex_ind),'.jpg'];
     results=frame2im(getframe(gcf));imwrite(results,name);
+    waitforbuttonpress;
 end
 
 display('Done');
